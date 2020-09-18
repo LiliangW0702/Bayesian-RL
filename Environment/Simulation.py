b704e124f7f7baab28cae95e9bc6ad5c3007ec29
LiliangW0702 <67132754+LiliangW0702@users.noreply.github.com>
2020-09-18 03:55:25 -0400

    fixed some bugs

--- Environment/Simulation.py
+++ Environment/Simulation.py
@@ -19,11 +19,13 @@ class Simulator:
         self.m=0.1
         self.g=9.8
         self.l=0.2
+        self.total_mass = self.m + self.M
 
         #system parameters
-        self.J=1/3*self.m*pow(self.l,2)
-        self.A=np.array([[0,1,0,0],[0,0,self.m*self.m*self.g*self.l*self.l/(self.J*(self.M+self.m)+self.M*self.m*self.l*self.l),0],[0,0,0,1],[0,0,self.m*self.g*self.l*(self.M+self.m)/(self.J*(self.M+self.m)+self.M*self.m*self.l*self.l),0]])
-        self.B=np.array([[0],[(self.J+self.m*self.l*self.l)/(self.J*(self.M+self.m)+self.M*self.m*self.l*self.l)],[0],[self.m*self.l/(self.J*(self.M+self.m)+self.M*self.m*self.l*self.l)]])
+        self.J = self.m * np.square(self.l) / 3.0
+        self.den = self.J * (self.total_mass) + self.M * self.m * self.l * self.l
+        self.A = np.array([[0, 1, 0, 0], [0, 0, -1.0 * self.m * self.m * self.g * self.l * self.l / self.den, 0], [0, 0, 0, 1],[0, 0, self.m * self.g * self.l * self.total_mass / self.den, 0]])
+        self.B = np.array([[0], [(self.J + self.m * self.l * self.l) / self.den], [0], [-1.0 * self.m * self.l / self.den]])
         self.H=np.identity(4)
         self.mean=[0,0,0,0]
         self.cov=[[0.1,0,0,0],[0,0.1,0,0],[0,0,0.1,0],[0,0,0,0.1]]
@@ -35,12 +37,12 @@ class Simulator:
         self.integrator = self.scipy_integration(self.simulate, self.get_state(), t_bound=self.time_span)
 
     def step(self, u):
-        self.current_action = u #np.array([u])
+        #self.current_action = u #np.array([u])
         while not (self.integrator.status == 'finished'):
             self.integrator.step()
         print("hi")
-        self.last_state = self.integrator.y #?
-        self.integrator = self.scipy_integration(self.simulate, self.get_state(), t0=self.integrator.t, t_bound=self.integrator.t+self.time_span)
+        self.last_state = self.integrator.y
+        self.integrator = self.scipy_integration(self.simulate, self.get_state(),t0=self.integrator.t, t_bound=self.integrator.t+self.time_span)
         return self.last_state
 
     def simulate(self, t,states):
@@ -48,18 +50,16 @@ class Simulator:
         :param local_states: Space state
         :return df_local_states
         """
-        x1 = states[0]
-        x2 = states[1]
-        x3 = states[2]
-        x4 = states[3]
-        x=np.array([[x1],[x2],[x3],[x4]])
+        x, x_dot, theta, theta_dot = states
+        s=np.array([[x],[x_dot],[theta],[theta_dot]])
 
-
+        
         # Derivative function
 
-        dx_dt=self.A*x+self.B*self.current_action+np.random.multivariate_normal(self.mean,self.cov,1)
+        ds_dt=np.matmul(self.A,s)+self.B*self.current_action+np.random.multivariate_normal(self.mean,self.cov,1)
+        x_dot, xacc, theta_dot, thetaacc = ds_dt[0, 0], ds_dt[1, 0], ds_dt[2, 0], ds_dt[3, 0]
+        return x_dot, xacc, theta_dot, thetaacc
 
-        return dx_dt
 
     def scipy_integration(self, fun, x0, t0=0, t_bound=10):
         return RK45(fun, t0, x0,t_bound, rtol=self.time_span/self.number_iterations, atol=1e-4)
